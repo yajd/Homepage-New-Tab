2a2e4ad19fd849eae3795eec2cc7e9a1c948b643
Noitidart <noitidart@gmail.com>
2014-06-27 23:54:24 -0700

    1.0.6.rev8
    boostrap.js
    - made note of the opening links within frame bug
    - converted to toggleDisplay function
    - takes into consideration when to use old method and when to use new
    - changed to using Services.jsm
    - also fixed bug where if have new tab tabs open and when install hnt
    then open new tab and toggle that, it will try to toggle in those old
    tabs which has no iframe so it would break

--- bootstrap.js
+++ bootstrap.js
@@ -1,12 +1,14 @@
 const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
-const wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
-const os = Cc['@mozilla.org/observer-service;1'].getService(Ci.nsIObserverService);
-const fm = Cc['@mozilla.org/focus-manager;1'].getService(Ci.nsIFocusManager);
-const sss = Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService);
-const ios = Cc['@mozilla.org/network/io-service;1'].getService(Ci.nsIIOService);
-const prefs = Cc['@mozilla.org/preferences-service;1'].getService(Ci.nsIPrefBranch);
+const sss = Cc['@mozilla.org/content/style-sheet-service;1'].getService(Ci.nsIStyleSheetService); //its needed right away on startup so no need to lazy load this
+
+
 var LMObserver;
 var appliedURI;
+var useNewMethod;
+var lastVersionToUseOldMethod = '30.*';
+
+Cu.import('resource://gre/modules/Services.jsm');
+Cu.import('resource://gre/modules/devtools/Console.jsm');
 
 function myObserver() {
 	this.register();
@@ -24,7 +26,7 @@ myObserver.prototype = {
 			}
 			subject.onreadystatechange = null;
 			var domDoc = subject;
-			var prevent_focus = prefs.getBoolPref('extensions.homepagenewtab.prevent_focus');
+			var prevent_focus = Services.prefs.getBoolPref('extensions.homepagenewtab.prevent_focus');
 			if (prevent_focus) {
 				var searchText = domDoc.querySelector('#searchText');
 				searchText.removeAttribute('autofocus');
@@ -35,10 +37,10 @@ myObserver.prototype = {
 				}
 			}
 
-			var gURLBar = wm.getMostRecentWindow("navigator:browser").gBrowser.getBrowserForDocument(subject).ownerDocument.defaultView.gURLBar;
+			var gURLBar = Services.wm.getMostRecentWindow("navigator:browser").gBrowser.getBrowserForDocument(subject).ownerDocument.defaultView.gURLBar;
 			var gURLBarUpped = function () {
 				Cu.reportError('HomepageNewTab - gURLBarUpped');
-				if (fm.focusedElement == subject.querySelector('html')) {
+				if (Services.focus.focusedElement == subject.querySelector('html')) {
 					if (subject.querySelector('#searchText')) {
 						searchText.focus();
 					}
@@ -78,10 +80,66 @@ myObserver.prototype = {
 				} else {
 					var xpc = iframe.contentWindow.wrappedJSObject;
 					var _createSiteFragment = xpc.uneval(xpc.gGrid._createSiteFragment);
-					xpc.eval('gGrid._createSiteFragment = ' + _createSiteFragment.replace('<a class="newtab-link">', '<a class="newtab-link" target="_parent">'));
+					xpc.eval('gGrid._createSiteFragment = ' + _createSiteFragment.replace('<a class="newtab-link">', '<a class="newtab-link" target="_parent">')); //this here was responsible for opening pages in new tab. we want to find alternative method as when user unpins or gets new thumnails in, if he clicks it, it opens the link within the frame
 				}
-				var toggleDisplays = function (tdDoc) {
-					iframe = tdDoc.querySelector('iframe');
+
+				toggleDisplay(domDoc);
+				
+				var toggleButton = iframe.contentDocument.querySelector('#newtab-toggle');
+				toggleButton.addEventListener('click', function () {
+					toggleDisplay(domDoc);															 
+					let windows = Services.wm.getEnumerator("navigator:browser"); //THIS GETS ALL BROWSER TYPE WINDOWS (MEANING IT HAS GBROWSER)
+					while (windows.hasMoreElements()) {
+						let browserWin = windows.getNext();
+						let domWindow = browserWin.QueryInterface(Ci.nsIDOMWindow);
+						var tabbrowser = browserWin.gBrowser;
+						var numTabs = tabbrowser.browsers.length;
+						for (var index = 0; index < numTabs; index++) {
+							var currentBrowser = tabbrowser.getBrowserAtIndex(index);
+							if (/^about\:home$/im.test(currentBrowser.currentURI.spec) && currentBrowser.contentDocument != domDoc) {
+								toggleDisplay(currentBrowser.contentDocument);
+							}
+						}
+
+					}
+				}, false);
+			}, false);
+		}
+
+	},
+	register: function () {
+		//Services.obs.addObserver(this, "passwordmgr-found-form", false);
+		Services.obs.addObserver(this, 'document-element-inserted', false);
+	},
+	unregister: function () {
+		//Services.obs.removeObserver(this, "passwordmgr-found-form");
+		Services.obs.removeObserver(this, 'document-element-inserted', false);
+	}
+};
+
+function toggleDisplay(tdDoc) {
+	console.info('useNewMethod~', useNewMethod);
+	if (useNewMethod === undefined) {
+		var platformVersion = Services.appinfo.platformVersion;
+		useNewMethod = Services.vc.compare(platformVersion, lastVersionToUseOldMethod); //will return 1 for anything greater than lastVersionToUseOldMethod. if its same then it will be 0 or if its less than it will be -1. 
+
+		if (useNewMethod > 0) {
+		  //use new method
+		  useNewMethod = true;
+		} else {
+		  //use old method
+		  useNewMethod = false;
+		}
+	}
+	
+	console.log('useNewMethod=', useNewMethod);
+	var iframe = tdDoc.querySelector('iframe');
+	if (!iframe) {
+		console.warn('no iframe in this new tab tab');
+		return;
+	}
+	if (!useNewMethod) {
+		//use old method
 					var spacers = tdDoc.querySelectorAll('.spacer');
 					var newToggleButton = tdDoc.querySelector('#newtab-toggle');
 					var logo = tdDoc.querySelector('#brandLogo');
@@ -100,7 +158,7 @@ myObserver.prototype = {
 						iframe.style.mozBoxFlex = 0;
 						iframe.style.height = '50px';
 						searchContainer.style.margin = '';
-						if (prefs.getBoolPref('extensions.homepagenewtab.hide_search_field')) {
+						if (Services.prefs.getBoolPref('extensions.homepagenewtab.hide_search_field')) {
 							var searchContainerChildren = searchContainer.childNodes;
 							for (var m=0; m<searchContainerChildren.length; m++) {
 								if (searchContainerChildren[m].style) {
@@ -121,7 +179,7 @@ myObserver.prototype = {
 						iframe.style.height = 'auto';
 						aboutMozilla.style.right = 'auto';
 						aboutMozilla.style.left = '12px';
-						if (prefs.getBoolPref('extensions.homepagenewtab.hide_search_field')) {
+						if (Services.prefs.getBoolPref('extensions.homepagenewtab.hide_search_field')) {
 							var searchContainerChildren = searchContainer.childNodes;
 							for (var m=0; m<searchContainerChildren.length; m++) {
 								if (searchContainerChildren[m].style) {
@@ -130,62 +188,99 @@ myObserver.prototype = {
 							}
 						}
 					}
+	} else {
+		//use new method
+		var gAllPagesEnabled = iframe.contentWindow.gAllPages.enabled;
+		var parent = iframe.ownerDocument;
+		if (gAllPagesEnabled) {
+			//show the thumnbails
+			var changesParent = {
+				'#topSection': {
+					style: null
 				}
-				toggleDisplays(domDoc);
-				
-				var toggleButton = iframe.contentDocument.querySelector('#newtab-toggle');
-				toggleButton.addEventListener('click', function () {
-					toggleDisplays(domDoc);															 
-					let windows = wm.getEnumerator("navigator:browser"); //THIS GETS ALL BROWSER TYPE WINDOWS (MEANING IT HAS GBROWSER)
-					while (windows.hasMoreElements()) {
-						let browserWin = windows.getNext();
-						let domWindow = browserWin.QueryInterface(Ci.nsIDOMWindow);
-						var tabbrowser = browserWin.gBrowser;
-						var numTabs = tabbrowser.browsers.length;
-						for (var index = 0; index < numTabs; index++) {
-							var currentBrowser = tabbrowser.getBrowserAtIndex(index);
-							if (/^about\:home$/im.test(currentBrowser.currentURI.spec) && currentBrowser.contentDocument != domDoc) {
-								toggleDisplays(currentBrowser.contentDocument);
-							}
-						}
-
+			}
+			var changesIframe = {
+				'#newtab-search-container': {
+					'page-disabled': null
+				}
+			}
+			for (var n in changesParent) {
+				for (var n2 in changesParent[n]) {
+					console.log('querySelectoring', n);
+					if (changesParent[n][n2] === null) {
+						parent.querySelector(n).removeAttribute(n2);
+					} else {
+						parent.querySelector(n).setAttribute(n2, changesParent[n][n2]);
 					}
-				}, false);
-			}, false);
+				}
+			}
+			for (var n in changesIframe) {
+				for (var n2 in changesIframe[n]) {
+					console.log('querySelectoring', n);
+					if (changesIframe[n][n2] === null) {
+						iframe.contentDocument.querySelector(n).removeAttribute(n2);
+					} else {
+						iframe.contentDocument.querySelector(n).setAttribute(n2, changesIframe[n][n2]);
+					}
+				}
+			}
+		} else {
+			//dont show the thumbnails
+			var changesParent = {
+				'#topSection': {
+					style: 'display:none'
+				}
+			};
+			var changesIframe = {
+				'#newtab-search-container': {
+					'page-disabled': 'true'
+				}
+			};
+			for (var n in changesParent) {
+				for (var n2 in changesParent[n]) {
+					console.log('querySelectoring', n);
+					if (changesParent[n][n2] === null) {
+						parent.querySelector(n).removeAttribute(n2);
+					} else {
+						parent.querySelector(n).setAttribute(n2, changesParent[n][n2]);
+					}
+				}
+			}
+			for (var n in changesIframe) {
+				for (var n2 in changesIframe[n]) {
+					console.log('querySelectoring', n);
+					if (changesIframe[n][n2] === null) {
+						iframe.contentDocument.querySelector(n).removeAttribute(n2);
+					} else {
+						iframe.contentDocument.querySelector(n).setAttribute(n2, changesIframe[n][n2]);
+					}
+				}
+			}
 		}
-
-	},
-	register: function () {
-		//os.addObserver(this, "passwordmgr-found-form", false);
-		os.addObserver(this, 'document-element-inserted', false);
-	},
-	unregister: function () {
-		//os.removeObserver(this, "passwordmgr-found-form");
-		os.removeObserver(this, 'document-element-inserted', false);
 	}
-};
+}
 
 function startup(aData, aReason) {
 	if (aReason == ADDON_INSTALL || aReason == ADDON_ENABLE || aReason == ADDON_DOWNGRADE || aReason == ADDON_UPGRADE) {
-		var url = prefs.setCharPref('browser.newtab.url', 'about:home');
+		var url = Services.prefs.setCharPref('browser.newtab.url', 'about:home');
 	}
 
 	if (aReason == ADDON_INSTALL || aReason == ADDON_UPGRADE || aReason == ADDON_ENABLE || aReason == ADDON_DOWNGRADE) {
 		try {
-			var existCheck_prevent_focus = prefs.getBoolPref('extensions.homepagenewtab.prevent_focus');
+			var existCheck_prevent_focus = Services.prefs.getBoolPref('extensions.homepagenewtab.prevent_focus');
 		} catch (ex) {
-			prefs.setBoolPref('extensions.homepagenewtab.prevent_focus', true);
+			Services.prefs.setBoolPref('extensions.homepagenewtab.prevent_focus', true);
 		}
 		
 		try {
-			var existCheck_prevent_focus = prefs.getBoolPref('extensions.homepagenewtab.hide_search_field');
+			var existCheck_prevent_focus = Services.prefs.getBoolPref('extensions.homepagenewtab.hide_search_field');
 		} catch (ex) {
-			prefs.setBoolPref('extensions.homepagenewtab.hide_search_field', false);
+			Services.prefs.setBoolPref('extensions.homepagenewtab.hide_search_field', false);
 		}
 	}
 
 	LMObserver = new myObserver;
-	appliedURI = ios.newURI(aData.resourceURI.spec + 'global.css', null, null);
+	appliedURI = Services.io.newURI(aData.resourceURI.spec + 'global.css', null, null);
 	sss.loadAndRegisterSheet(appliedURI, sss.USER_SHEET);
 
 }
@@ -196,7 +291,7 @@ function shutdown(aData, aReason) {
 	LMObserver.unregister();
 	sss.unregisterSheet(appliedURI, sss.USER_SHEET);
 	if (aReason == ADDON_UNINSTALL || aReason == ADDON_DISABLE) {
-		var url = prefs.clearUserPref('browser.newtab.url');
+		var url = Services.prefs.clearUserPref('browser.newtab.url');
 	}
 
 }
